60816dcaf37783241da3dcbda07dbd8641c49008
mostafaElkenany <mostafa.elkenany@gmail.com>
2020-07-01 04:08:17 +0200

    -Implemented redis database to store/get login dates for users -added /search route to get login dates for input username

--- routes/user.js
+++ routes/user.js
@@ -1,8 +1,21 @@
 const express = require('express');
+const redis = require('redis');
+const _ = require('lodash');
+const moment = require('moment');
 const auth = require('../middlewares/auth');
 const User = require('../models/user');
 
 const router = express.Router();
+
+// creating redis client
+const client = redis.createClient();
+client.on("connect", () => {
+    console.log('Conncted to redis...');
+
+});
+client.on("error", (error) => {
+    console.error(error);
+});
 
 // adding routes to express router
 router.get('', auth, (req, res) => {
@@ -19,6 +32,16 @@ router.post('/login', (req, res) => {
         .then(function (user) {
             if (user && user.validatePssword(password)) {
                 req.session.user = user.dataValues;
+
+                const username = user.dataValues.username
+                const date = new Date();
+
+                //store user login dates in redis
+                client.lpush(username, date, (err, res) => {
+                    if (err) console.log(err);
+                    else console.log(res);
+                });
+
                 res.redirect('/home');
             }
             else res.render('login', { error: 'Wrong email or password' })
@@ -56,6 +79,25 @@ router.get('/home', (req, res) => {
         res.render('home');
     }
     else res.redirect('/login');
+})
+
+//route to get login dates
+router.get('/search', (req, res) => {
+    // console.log(req.query);
+    const { username } = req.query;
+
+    //get login dates from redis
+    client.lrange(username, 0, -1, (err, result) => {
+        if (err) console.log(err);
+        else {
+            // group login dates by day
+            const groupedDates = _.groupBy(result, (date) => {
+                return moment(date, "MM-DD-YYYY").startOf('day').format("dddd, MMMM Do YYYY");
+            })
+
+            res.render('home', { groupedDates });
+        };
+    })
 })
 
 router.get('/logout', (req, res) => {
